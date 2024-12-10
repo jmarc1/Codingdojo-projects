a87e9256782f9cb9e8e3d7f602e3270c6746f9ea
jmarc1 <jeffmarc07@yahoo.com>
2024-12-10 09:44:09 -0500

    updated at home

--- book_club/flask_app/controllers/login_control.py
+++ book_club/flask_app/controllers/login_control.py
@@ -1,11 +1,12 @@
 from flask_app import app
 # from flask_app.config.myConnection import connect_to_mysql
-from flask_app.models.login_models import logins
+from flask_app.models.login_model import logins
 from flask_bcrypt import Bcrypt
-
+from flask import flash,render_template,redirect,flash,request
 
 @app.route("/login", methods=["POST"])
 def login():
     data = {
         "email":request.form["email"]
     }
+    return render_template("dashboard.html")
